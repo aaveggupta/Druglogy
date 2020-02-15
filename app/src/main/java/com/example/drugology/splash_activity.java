3b344244f25f811655d972e55eecbcc21dc11f84
Aaveg Gupta <aaveggupta.work@gmail.com>
2020-02-15 11:56:41 +0530

    added splash screen

---
+++ app/src/main/java/com/example/drugology/splash_activity.java
@@ -0,0 +1,22 @@
+package com.example.drugology;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+public class splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+
+        SystemClock.sleep(2000);
+        Intent intent = new Intent(splash_activity.this,first_page.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
